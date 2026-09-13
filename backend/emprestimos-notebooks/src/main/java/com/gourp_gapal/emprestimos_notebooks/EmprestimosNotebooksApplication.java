b9f6afc10744dc4fa5fb6e77b6db966865026596
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-09-13 19:13:13 -0300

    feat: criado o projeto springboot com o .zip original na pasta bakcend. criado o arquivo dockercompose que esta subindo postgresql 16

---
+++ backend/emprestimos-notebooks/src/main/java/com/gourp_gapal/emprestimos_notebooks/EmprestimosNotebooksApplication.java
@@ -0,0 +1,13 @@
+package com.gourp_gapal.emprestimos_notebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimosNotebooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimosNotebooksApplication.class, args);
+	}
+
+}
---
+++ backend/emprestimos-notebooks/src/test/java/com/gourp_gapal/emprestimos_notebooks/EmprestimosNotebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourp_gapal.emprestimos_notebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimosNotebooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
